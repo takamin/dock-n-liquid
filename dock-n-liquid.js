c35c907c8ee510fa3db35d064f8582a29645cd7e
Koji Takami <vzg03566@gmail.com>
2017-04-10 16:25:54 +0900

    Add basic features
    * Set element style position absolute.
    * Automatic detection for classes `container` and `content`.
    * Do not align the elements having the styles display:node or visibility:hidden.

--- dock-n-liquid.js
+++ dock-n-liquid.js
@@ -7,15 +7,24 @@
             handler.call(obj, arr[i], i, arr);
         }
     }
+    var DOCK_DIR = [ 'top', 'left', 'right', 'bottom' ];
+    function LayoutElement(element) {
 
-    function LayoutElement(element, rect) {
-        element.style["box-sizing"] = "border-box";
         this._element = element;
         this._containerRect = null;
-        this.updateRect(rect);
+        this._isContent = null;
+        this._isContainer = null;
 
-        ['container', 'top', 'left', 'right', 'bottom', 'content'].
-        forEach(function(clsnam) {
+        this.updateRect();
+
+        this._element.style["box-sizing"] = "border-box";
+        this._element.style["position"] = "absolute";
+        this._element.style["top"] = "0";
+        this._element.style["left"] = "0";
+        this._element.style["right"] = "0";
+        this._element.style["bottom"] = "0";
+
+        DOCK_DIR.forEach(function(clsnam) {
             var f = [
                 "_is", clsnam.charAt(0).toUpperCase(),
                 clsnam.slice(1)
@@ -23,28 +32,33 @@
             this[f] = element.classList.contains(clsnam);
         }, this);
 
-        this._children = LayoutElement.parseChildren(element);
-    }
-    LayoutElement.prototype.updateRect = function(rect) {
-        if(rect == null) {
-            rect = LayoutElement.getInnerRect(this._element);
+        if(!this._isTop && !this._isLeft && !this._isRight && !this._isBottom) {
+            this._isContent = true;
         }
-        this._containerRect = rect;
-    };
-    LayoutElement.getInnerRect = function(element) {
-        if(element === window) {
-            return new BBox.Rect(0, 0,
+
+        this._children = LayoutElement.parseChildren(element);
+        if(this._children.length > 0) {
+            this._isContainer = true;
+        }
+    }
+    LayoutElement.prototype.updateRect = function() {
+        if(this._element === document.body) {
+            var wndRect = new BBox.Rect(0, 0,
                     parseInt(window.innerWidth),
                     parseInt(window.innerHeight));
+            var bboxBody = new BBox(document.body);
+            wndRect.bottom -= bboxBody.marginVerticalNc();
+            this._containerRect = wndRect;
+        } else {
+            var bbox = new BBox(this._element);
+            this._containerRect = BBox.Rect.fromBBox(bbox);
         }
-        var bbox = new BBox(element);
-        return BBox.Rect.fromBBox(bbox);
     };
 
     LayoutElement.parseChildren = function(element) {
         var dock_elements = element.getElementsByClassName('dock');
         var result = [];
-        if(dock_elements) {
+        if(dock_elements && dock_elements.length > 0) {
             each(dock_elements, function(dock_element) {
                 if(dock_element.parentNode === element) {
                     result.push(new LayoutElement(dock_element));
@@ -61,27 +75,51 @@
             var childWidth = bboxChild.px("width") + bboxChild.marginHorizontalNc();
             var childHeight = bboxChild.px("height") + bboxChild.marginVerticalNc();
             child.setBound(rect);
+            if(child._element.style.display == "none" ||
+            child._element.style.visibility == "hidden")
+            {
+                return;
+            }
             if(child._isTop) {
                 child._element.style.bottom = rect.top + childHeight + "px";
+                var bboxChild = new BBox(child._element);
+                child._element.style.width =
+                    ((rect.right - rect.left)
+                     - bboxChild.marginHorizontalNc()) + "px";
                 rect.top += childHeight;
             } else if(child._isLeft) {
                 child._element.style.right = rect.left + childWidth + "px";
+                var bboxChild = new BBox(child._element);
+                child._element.style.height =
+                    ((rect.bottom - rect.top)
+                     - bboxChild.marginVerticalNc()) + "px";
                 rect.left += childWidth;
             } else if(child._isRight) {
                 child._element.style.left = rect.right - childWidth + "px";
+                var bboxChild = new BBox(child._element);
+                child._element.style.height =
+                    ((rect.bottom - rect.top)
+                     - bboxChild.marginVerticalNc()) + "px";
                 rect.right -= childWidth;
             } else if(child._isBottom) {
                 child._element.style.top = rect.bottom - childHeight + "px";
+                var bboxChild = new BBox(child._element);
+                child._element.style.width =
+                    ((rect.right - rect.left)
+                     - bboxChild.marginHorizontalNc()) + "px";
                 rect.bottom -= childHeight;
-            } else if(!child._isContent) {
-                throw new Error("No class specified for docking direction or cotnent");
             }
         }, this);
         this._children.forEach(function (child) {
             if(child._isContent) {
                 child.setBound(rect);
                 var bboxChild = new BBox(child._element);
-                child._element.style.height = ((rect.bottom - rect.top) - bboxChild.marginVerticalNc()) + "px";
+                child._element.style.width =
+                    ((rect.right - rect.left)
+                     - bboxChild.marginHorizontalNc()) + "px";
+                child._element.style.height =
+                    ((rect.bottom - rect.top)
+                     - bboxChild.marginVerticalNc()) + "px";
             }
             child.updateRect();
         });
